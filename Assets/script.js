a02f826a927f34331af2a104a736d6d79c158a84
Kyle <schoenbergerk@hotmail.com>
2024-02-24 12:29:40 -0600

    finished the output portion of the generator to display characters

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,8 @@
 // Possible characters for password
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-var specialChars = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialChars = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
 
 
 
@@ -10,41 +10,61 @@ var specialChars = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', 
 var generateBtn = document.querySelector("#generate");
 
 
-// Collect user inputs
-function userInput() {
-  var length = prompt("How many characters do your password need to be?");
+// Builds password
+function generatePassword() {
+  var passWordLength = prompt("How many characters do your password need to be?");
   console.log(length);
-  while (length < 8 || length > 128) {
+  while (passWordLength < 8 || passWordLength > 128) {
     alert("Please pick a number between 8 and 128")
     var length = prompt("How many characters do your password need to be?");
   }
 
-var isLowerCase = confirm("Do you need lowercase letters?")
-console.log(isLowerCase);
-var isUpperCase = confirm("Do you need uppercase letters?")
-console.log(isUpperCase);
-var isNumbers = confirm("Do you need numbers?")
-console.log(isNumbers);
-var isSpecialChars = confirm("Do you need special characters?")
-console.log(isSpecialChars);
-
-while (isLowerCase === false && isUpperCase === false && isNumbers === false && isSpecialChars === false) {
-  alert("You must choose at least one character option")
   var isLowerCase = confirm("Do you need lowercase letters?")
   var isUpperCase = confirm("Do you need uppercase letters?")
   var isNumbers = confirm("Do you need numbers?")
   var isSpecialChars = confirm("Do you need special characters?")
+  // console.log(isLowerCase);
+  // console.log(isUpperCase);
+  // console.log(isNumbers);
+  // console.log(isSpecialChars);
+
+// Loops through options if user declines all prompts without needing a refresh
+  while (isLowerCase === false && isUpperCase === false && isNumbers === false && isSpecialChars === false) {
+    alert("You must choose at least one character option")
+    var isLowerCase = confirm("Do you need lowercase letters?")
+    var isUpperCase = confirm("Do you need uppercase letters?")
+    var isNumbers = confirm("Do you need numbers?")
+    var isSpecialChars = confirm("Do you need special characters?")
+  }
+
+  var userInput = []
+
+  if (isLowerCase === true) {
+    userInput = userInput.concat(lowerCase)
+  }
+
+  if (isUpperCase === true) {
+    userInput = userInput.concat(upperCase)
+  }
+
+  if (isNumbers === true) {
+    userInput = userInput.concat(numbers)
+  }
+
+  if (isSpecialChars === true) {
+    userInput = userInput.concat(specialChars)
+  }
+  console.log(userInput);
+
+  buildPassword = ""
+  
+// Runs until the length has been reached
+  for (let i = 0; i < passWordLength; i++) {
+    buildPassword = buildPassword + userInput[Math.floor(Math.random() * userInput.length)];
+    console.log(buildPassword);
+  }
+  return buildPassword;
 }
-
-}
-
-
-
-// Take user inputs and generate password
-function generatePassword() {
-  var options = userInput();
-}
-
 
 
 // Write password to the #password input
