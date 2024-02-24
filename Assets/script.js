fe492e041d34c378b880f10ba6395d5b21809bd7
Kyle <schoenbergerk@hotmail.com>
2024-02-24 09:50:00 -0600

    Added potential characters

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,27 @@
+// Possible characters for password
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+var specialChars = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 
 // Collect user inputs
-
+function userInput() {
+  var length = prompt("How many characters do your password need to be?");
+  console.log(length);
+}
 
 
 
 // Take user inputs and generate password
-
+function generatePassword(){
+  var options = userInput();
+}
 
 
 
